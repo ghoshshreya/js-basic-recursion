1ef0fd0e55012e26018ad913759192fdb8566413
ghoshshreya <shrghosh08@gmail.com>
2023-03-10 17:30:20 +0530

    2 files deleted

--- printNTimes.js
+++
@@ -1,5 +0,0 @@
-var printNTimes = function (text, numOfTimes) {
-  // while(numOfTimes > 0) {
-  //   printNTimes
-  // }
-};
--- printNumbers.js
+++
@@ -1,8 +0,0 @@
-function printNumbers(num) {
-  console.log(num);
-  if (num > 1) {
-    printNumbers(num - 1);
-  }
-}
-
-printNumbers(10);
