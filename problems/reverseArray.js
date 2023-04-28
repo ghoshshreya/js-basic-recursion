1b46a5637203f3974744fbf1f71efc56988a88ca
ghoshshreya <shrghosh08@gmail.com>
2023-04-29 00:26:31 +0530

    1 file modified

--- problems/reverseArray.js
+++ problems/reverseArray.js
@@ -1,17 +1,14 @@
-// Reverse an array using recursion
-// I/P - [1, 2, 3, 4, 5]
-// O/P - [5, 4, 3, 2, 1]
-const reverseArray = function (arr) {
-  let n = arr.length;
-  function reverse(arr, i, j) {
-    if (i < j) {
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-      return reverse(arr, i + 1, j - 1);
-    } else return arr;
+function reverseArray(arr) {
+  function reverse(arr, first, last) {
+    if (first >= last) return;
+    const temp = arr[first];
+    arr[first] = arr[last];
+    arr[last] = temp;
+    console.log(arr);
+    reverse(arr, first + 1, last - 1);
   }
-  return reverse(arr, 0, n - 1);
-};
+  reverse(arr, 0, arr.length - 1);
+  return arr;
+}
 
-reverseArray([1, 2, 3, 4, 5, 6]);
+console.log(reverseArray([1, 2, 3]));
